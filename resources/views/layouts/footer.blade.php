729905971e31351ed9f2eff81d10ae5c5b817f1b
Anthonyca18m <anthonyca18m@gmail.com>
2020-12-30 18:29:39 -0500

    Cambiando a latino, cambiando footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,82 +1,82 @@
 <footer>
     <div class="container">
-      <div class="footer-content">
-        <div class="row">
-          <div class="col-12 col-md-6 col-lg-4">
-            <div class="footer-col -about">
-              <div class="center-line-title"> 
-                <h5>About us</h5>
-              </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida lacus vel facilisis. </p>
-              <div class="contact-method">
-                <p> <i class="fas fa-map-marker-alt"></i>5 South Main Street Los Angeles, ZZ-96110 USA</p>
-                <p> <i class="far fa-mobile-android"></i>125-711-811   |   125-668-886</p>
-                <p> <i class="fas fa-headphones-alt"></i>Support.hosting@gmail.com</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4">
-            <div class="footer-col -feature-post">
-              <div class="center-line-title"> 
-                <h5>Feature posts</h5>
-              </div>
-              <div class="feature-post-block">
-                    <div class="post-card -tiny"><a class="card__cover" href="post_standard.html"><img src="images/posts/1.png" alt="Looking for some feedback for this rejected track"/></a>
-                      <div class="card__content">
-                        <h5 class="card__content-category">Technology</h5><a class="card__content-title" href="post_standard.html">Looking for some feedback for this rejected track</a>
-                        <div class="card__content-info">
-                          <div class="info__time"><i class="far fa-clock"></i>
-                            <p>Clock  Wed 02, 2019</p>
-                          </div>
+        <div class="footer-content">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="footer-col -about">
+                        <div class="center-line-title">
+                            <h5>Sobre Nosotros</h5>
                         </div>
-                      </div>
-                    </div>
-                    <div class="post-card -tiny"><a class="card__cover" href="post_standard.html"><img src="images/posts/2.png" alt="How to name, save and export a finish template"/></a>
-                      <div class="card__content">
-                        <h5 class="card__content-category">Typography</h5><a class="card__content-title" href="post_standard.html">How to name, save and export a finish template</a>
-                        <div class="card__content-info">
-                          <div class="info__time"><i class="far fa-clock"></i>
-                            <p>Clock  Wed 02, 2019</p>
-                          </div>
+                        <p>Este proyecto tiene como objetivo mejorar la calidad de medio ambiente en Lima - Perú</p>
+                        <div class="contact-method">
+                            <p> <i class="fas fa-map-marker-alt"></i>Av Tesis con IDAT - a lado de UTP</p>
+                            <p> <i class="far fa-mobile-android"></i>911 111 111 - 922 222 222</p>
+                            <p> <i class="fas fa-headphones-alt"></i>developer@goodjob.com</p>
                         </div>
-                      </div>
                     </div>
-                    <div class="post-card -tiny"><a class="card__cover" href="post_standard.html"><img src="images/posts/3.png" alt="I want to create a logo with illustrator hacker"/></a>
-                      <div class="card__content">
-                        <h5 class="card__content-category">Graphic</h5><a class="card__content-title" href="post_standard.html">I want to create a logo with illustrator hacker</a>
-                        <div class="card__content-info">
-                          <div class="info__time"><i class="far fa-clock"></i>
-                            <p>Clock  Wed 02, 2019</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-md-12 col-lg-4">
-            <div class="footer-col -util">
-              <div class="row">
-                <div class="col-12 col-md-6 col-lg-12">
-                  <div class="center-line-title"> 
-                    <h5>Tag clouds</h5>
-                  </div>
-                  <div class="tags-group"><a class="tag-btn" href="blog_category_grid.html">Gutenews</a><a class="tag-btn" href="blog_category_grid.html">Lifestyle</a><a class="tag-btn" href="blog_category_grid.html">Fashion</a><a class="tag-btn" href="blog_category_grid.html">Technology</a><a class="tag-btn" href="blog_category_grid.html">Food</a><a class="tag-btn" href="blog_category_grid.html">Travel</a>
-                  </div>
                 </div>
-                <div class="col-12 col-md-6 col-lg-12">
-                  <div class="center-line-title"> 
-                    <h5>Follow us</h5>
-                  </div>
-                  <div class="social-block"><a href="#"><i class="fab fa-facebook-f"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-instagram"></i></a><a href="#"><i class="fab fa-dribbble"></i></a></div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="footer-col -feature-post">
+                        <div class="center-line-title">
+                            <h5>Feature posts</h5>
+                        </div>
+                        <div class="feature-post-block">
+                            <div class="post-card -tiny"><a class="card__cover" href="post_standard.html"><img src="images/posts/1.png" alt="Looking for some feedback for this rejected track" /></a>
+                                <div class="card__content">
+                                    <h5 class="card__content-category">Technology</h5><a class="card__content-title" href="post_standard.html">Looking for some feedback for this rejected track</a>
+                                    <div class="card__content-info">
+                                        <div class="info__time"><i class="far fa-clock"></i>
+                                            <p>Clock Wed 02, 2019</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="post-card -tiny"><a class="card__cover" href="post_standard.html"><img src="images/posts/2.png" alt="How to name, save and export a finish template" /></a>
+                                <div class="card__content">
+                                    <h5 class="card__content-category">Typography</h5><a class="card__content-title" href="post_standard.html">How to name, save and export a finish template</a>
+                                    <div class="card__content-info">
+                                        <div class="info__time"><i class="far fa-clock"></i>
+                                            <p>Clock Wed 02, 2019</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="post-card -tiny"><a class="card__cover" href="post_standard.html"><img src="images/posts/3.png" alt="I want to create a logo with illustrator hacker" /></a>
+                                <div class="card__content">
+                                    <h5 class="card__content-category">Graphic</h5><a class="card__content-title" href="post_standard.html">I want to create a logo with illustrator hacker</a>
+                                    <div class="card__content-info">
+                                        <div class="info__time"><i class="far fa-clock"></i>
+                                            <p>Clock Wed 02, 2019</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="footer-col -util">
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-lg-12">
+                                <div class="center-line-title">
+                                    <h5>Tag clouds</h5>
+                                </div>
+                                <div class="tags-group"><a class="tag-btn" href="blog_category_grid.html">Gutenews</a><a class="tag-btn" href="blog_category_grid.html">Lifestyle</a><a class="tag-btn" href="blog_category_grid.html">Fashion</a><a class="tag-btn" href="blog_category_grid.html">Technology</a><a class="tag-btn" href="blog_category_grid.html">Food</a><a class="tag-btn" href="blog_category_grid.html">Travel</a>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-12">
+                                <div class="center-line-title">
+                                    <h5>Follow us</h5>
+                                </div>
+                                <div class="social-block"><a href="#"><i class="fab fa-facebook-f"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-instagram"></i></a><a href="#"><i class="fab fa-dribbble"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="copyright">
-        <p>© 2019, GuteNews - News Magazine WordPress Theme. All rights reserved.</p>
-      </div>
+        <div class="copyright">
+            <p>© {{ now()->year }}, Goodjob - Por una mejor calidad de vida. Todos los derechos reservados.</p>
+        </div>
     </div>
-  </footer>
+</footer>
