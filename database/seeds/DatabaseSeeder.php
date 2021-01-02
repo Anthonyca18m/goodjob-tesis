5dfc49792a319fca1848827df81c56406e6af4ea
Anthonyca18m <anthonyca18m@gmail.com>
2021-01-02 13:00:52 -0500

    Seeder de provincias, departamentos, falta distritos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call([
+            DepartmentSeeder::class,
+            ProvinceSeeder::class,
+            DistrictSeeder::class
+        ]);
     }
 }
