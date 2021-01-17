81c2de091169b87414b250c2ba6898e02ec31c4d
anthonyca18m@ <anthonyca18m@gmail.com>
2021-01-17 00:26:47 -0500

    add all

--- public/js/app.js
+++ public/js/app.js
@@ -80499,8 +80499,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Carloz\Desktop\goodjob-tesis\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Carloz\Desktop\goodjob-tesis\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\goodjob-tesis\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\goodjob-tesis\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
